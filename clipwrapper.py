d73846f5e9f39d8f451c0e53afcf960e070a04d2
Yue Ma <mayueanyou@gmail.com>
2024-11-07 15:06:23 -0500

    update

---
+++ clipwrapper.py
@@ -0,0 +1,103 @@
+import os,sys,torch,pickle,pathlib
+from tqdm import tqdm
+from .similarity import SimilarityCalculator
+from .local.clip import* 
+
+class ClipTransform:
+    def __init__(self,model_sel) -> None:
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.available_models = ['RN50', 'RN101', 'RN50x4', 'RN50x16', 'RN50x64','ViT-B/16','ViT-B/32', 'ViT-L/14', 'ViT-L/14@336px']
+        self.model_name = self.available_models[model_sel]
+        _, self.preprocess = clip.load(self.model_name, self.device)
+
+    def __call__(self, pic):
+        pic = self.preprocess(pic)
+        return pic
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}()"
+
+class ClipWrapper():
+    def __init__(self,model_sel=8,classes=[''],base_text='',generate_text=True) -> None:
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model_sel = model_sel
+        self.available_models = ['RN50', 'RN101', 'RN50x4', 'RN50x16', 'RN50x64', 'ViT-B/16', 'ViT-B/32', 'ViT-L/14', 'ViT-L/14@336px']
+        #print(clip.available_models())
+        self.parameters = ['38,316,896','56,259,936','87,137,080','167,328,912','420,380,352','86,192,640','87,849,216','303,966,208','304,293,888']
+        self.model_name = self.available_models[model_sel]
+        print("Clip: " + self.model_name)
+        self.model, self.preprocess = clip.load(self.model_name, self.device)
+        total_params = sum(p.numel() for p in self.model.visual.parameters())
+        print(f'total parameters: {total_params:,}')
+        self.classes = classes
+        self.base_text = base_text
+        print('base text: ' + self.base_text)
+        self.similarity_calculator = SimilarityCalculator()
+        if generate_text: self.generate_text_features()
+    
+    def generate_text_features(self):
+        print('classes:')
+        print(self.classes)
+        text_inputs = torch.cat([clip.tokenize(self.base_text + ' ' + c) for c in self.classes]).to(self.device)
+        with torch.no_grad(): self.text_features = self.model.encode_text(text_inputs)
+        self.text_features /= self.text_features.norm(dim=-1, keepdim=True)
+    
+    def generate_img_features(self,images):
+        image_input = images.to(self.device) if len(images.shape) == 4 else images.unsqueeze(0).to(self.device)
+        with torch.no_grad(): image_features = self.model.encode_image(image_input)
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+        return image_features
+    
+    def get_predictions_softmax(self,images,topk=1):
+        image_features = self.generate_img_features(images)
+        image_features = image_features[:, None, :]
+        similarity = torch.abs(image_features - self.text_features)
+        similarity = similarity.sum(dim=-1)
+        similarity = torch.neg(similarity)
+        values, indices = similarity.topk(topk)
+        return values, indices
+    
+    def get_predictions(self,images,topk=1):
+        image_features = self.generate_img_features(images)
+        similarity = (100.0 * image_features @ self.text_features.T).softmax(dim=-1)
+        values, indices = similarity.topk(topk)
+        return values, indices
+    
+    def print_result(self,images):
+        values, indices = self.get_predictions(images,topk=len(self.classes))
+        values, indices = values[0], indices[0]
+        print("\nTop predictions:\n")
+        for value, index in zip(values, indices):
+            print(f"{self.classes[index]:>16s}: {100 * value.item():.2f}%")
+    
+    def eval_dataset_text(self,dataset):
+        acc,nums = 0,0
+        for images, labels in tqdm(dataset):
+            values, indices = self.get_predictions(images)
+            indices = torch.flatten(indices)
+            result = torch.eq(labels.to(self.device),indices.to(self.device))
+            acc += torch.sum(result)
+            nums += len(labels)
+        acc = acc.to(torch.float) / nums
+        print(acc)
+        return acc
+    
+    def inference_dataset(self,dataset):
+        data  = {'data':[],'targets':[]}
+        for images, labels in tqdm(dataset):
+            img_features = self.generate_img_features(images)
+            
+            data['data'].append(img_features.cpu().type(torch.float))
+            data['targets'].append(labels.cpu().type(torch.long))
+        
+        data['data'] = torch.cat(data['data'],0)
+        data['targets'] = torch.cat(data['targets'],0)
+        return data
+
+if __name__ == '__main__':
+    cw = ClipWrapper(8)
+    #test = clip.tokenize('ff adsf fdas')
+    #image = torch.rand((3,224,224))
+    #image = cw.preprocess(image)
+    #cw.generate_img_features(image)
+    
